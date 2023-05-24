ab63fd6d6a93c8929fb28e638b9f58d1293f9232
mihir <mihirparmar031@gmail.com>
2023-05-24 17:21:13 +0530

    formatted code

--- src/com/exception/Main.java
+++ src/com/exception/Main.java
@@ -4,22 +4,19 @@ public class Main {
 
   public static void main(String[] args) {
 
-    int number1=10;
-    int number2=0;
+    int number1 = 10;
+    int number2 = 0;
 
-     try{
-       int result=number1/number2;
-       System.out.println(result);
-     }
-     catch (ArithmeticException e){
-       System.out.println("You should not divide the number by zero !!");
-     }
-     catch (Exception e){
-       System.out.println("Exception occured");
-     }
-     finally {
-       System.out.println("finally block is executed");
-     }
+    try {
+      int result = number1 / number2;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("You should not divide the number by zero !!");
+    } catch (Exception e) {
+      System.out.println("Exception occured");
+    } finally {
+      System.out.println("finally block is executed");
+    }
 
   }
 }
